5b8b3180b01c99229ca0fc83fa601566304eb01e
jyoti254 <bit16cs18@bit.ac.in>
2021-05-31 13:02:27 +0530

    first modified commit

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -7,10 +7,11 @@ const Card=(props)=>{
         
                     <div className ="col-md-4 col-10 mx-auto">
                             <div className ="card">
-                                <img src={props.imgsrc} className ="card-img-top" alt={props.imgsrc}/>
+                                <img src={props.imgsrc} className ="card-img-top" alt={props.imgsrc} />
                                 <div className ="card-body">
                                     <h5 className ="card-title">{props.title}</h5>
                                     <p className ="card-text">{props.text}</p>
+                                    <button className="btn" type="submit">view more</button>
                                     <NavLink to="/about" ></NavLink>
                                 </div>
                             </div>
